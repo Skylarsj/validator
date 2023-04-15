9e6b19830b3d1e5496abe74ea7cf7cb2eda2cac8
Skylarsj <skylarsj@icloud.com>
2023-04-15 15:23:59 -0700

    update

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -57,12 +57,12 @@ const UserForm = (props) => {
             </form>
             
             <div>
-                <p>Live Feed</p>
+                {/* <p>Live Feed</p>
                 <p>First Name: {firstName}</p>
                 <p>Last Name: {lastName}</p>
                 <p>Email: {email}</p>
                 <p>Password: {password}</p>
-                <p>Confirm Password: {confirmPassword}</p>
+                <p>Confirm Password: {confirmPassword}</p> */}
             </div>
         </div>
     );
