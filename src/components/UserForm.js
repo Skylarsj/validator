7ff619802131d87573b36c33dfc096ccf4346f40
Skylarsj <skylarsj@icloud.com>
2023-04-15 15:53:44 -0700

    Finished

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -21,48 +21,46 @@ const UserForm = (props) => {
 
         e.preventDefault();
 
-        //setHasBeenSubmitted(true);
-
     };
-    
     return(
         <div>
             <form onSubmit={ createUser }>
-            //{
-               // hasBeenSubmitted ? 
-               // <h3>Thank you for submitting the form!</h3> :
-                //<h3>Welcome, please submit the form.</h3> 
-            }
                 <div>
                     <label>First Name: </label> 
                     <input class="w-30 m-3 border border-sky-500" id="firstName" name="firstName" type="text" onChange={createUser} />
+                    {form.firstName && form.firstName.length < 3 ? <p>Name must be longer than 3 characters</p>: null}
                 </div>
                 <div>
-                    <label>First Name: </label> 
-                    <input class="w-30 m-3 border border-sky-500" id="lasName" name="lasName" type="text" onChange={createUser} />
+                    <label>Last Name: </label> 
+                    <input class="w-30 m-3 border border-sky-500" id="lasName" name="lastName" type="text" onChange={createUser} />
+                    {form.lastName && form.lastName.length < 3 ? <p>Last Name must be longer than 3 characters</p>: null}
                 </div>
                 <div>
-                    <label>First Name: </label> 
+                    <label>Email Name: </label> 
                     <input class="w-30 m-3 border border-sky-500" id="email" name="email" type="text" onChange={createUser} />
+                    {form.email && form.email.length < 3 ? <p>Email must be longer than 3 characters</p>: null}
                 </div>
                 <div>
-                    <label>First Name: </label> 
+                    <label>Password: </label> 
                     <input class="w-30 m-3 border border-sky-500" id="passowrd" name="password" type="text" onChange={createUser} />
+                    {form.password && form.password.length < 3 ? <p>Password must be longer than 3 characters</p>: null}
                 </div>
                 <div>
-                    <label>First Name: </label> 
-                    <input class="w-30 m-3 border border-sky-500" id="confrimPassword" name="confirmPassword" type="text" onChange={createUser} />
+                    <label>Confirm Password: </label> 
+                    <input class="w-30 m-3 border border-sky-500" id="confirmPassword" name="confirmPassword" type="text" onChange={createUser} />
+                    {form.password !== form.confirmPassword ? <p>Passwords Must Match!
+                    </p>: null}
                 </div>
                 <input class="rounded-full w-35 m-3 border border-sky-500" type="submit" value="Create User" />
             </form>
             
             <div>
-                {/* <p>Live Feed</p>
-                <p>First Name: {firstName}</p>
-                <p>Last Name: {lastName}</p>
-                <p>Email: {email}</p>
-                <p>Password: {password}</p>
-                <p>Confirm Password: {confirmPassword}</p> */}
+                <p>Live Feed</p>
+                <p>First Name: {form.firstName}</p>
+                <p>Last Name: {form.lastName}</p>
+                <p>Email: {form.email}</p>
+                <p>Password: {form.password}</p>
+                <p>Confirm Password: {form.confirmPassword}</p>
             </div>
         </div>
     );
